51cb1eff62281196f94252689a75be961b3c6ec8
pkErbynn <john.erbynn@gmail.com>
2019-11-21 10:18:02 +0000

    some mapping

--- src/main/java/io/turntable/projectwebservice/controllers/ProjectController.java
+++ src/main/java/io/turntable/projectwebservice/controllers/ProjectController.java
@@ -1,4 +1,21 @@
 package io.turntable.projectwebservice.controllers;
 
+import io.turntable.projectwebservice.models.Project;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProjectController {
+
+    @RequestMapping("/project")
+    public Project getAllProject() {
+        return null;
+    }
+
+    @RequestMapping("/project/{}")
+    public Project getAllProjectByName() {
+        return null;
+    }
+
+
 }
