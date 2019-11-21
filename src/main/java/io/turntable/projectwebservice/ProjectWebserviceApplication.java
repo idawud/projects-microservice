3be39ed7139536d9bcf8c6eca7cb76a9ec3529be
pkErbynn <john.erbynn@gmail.com>
2019-11-21 09:43:41 +0000

    init

---
+++ src/main/java/io/turntable/projectwebservice/ProjectWebserviceApplication.java
@@ -0,0 +1,13 @@
+package io.turntable.projectwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectWebserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectWebserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/turntable/projectwebservice/ProjectWebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.turntable.projectwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectWebserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
